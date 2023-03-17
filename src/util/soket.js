f28d1750e18d5605f2fa46d5237775a82c8759cd
unknown <penpenetai@icloud.com>
2023-03-16 23:33:39 -0700

    inisitated

---
+++ src/util/soket.js
@@ -0,0 +1,41 @@
+module.exports = (http)=>{
+    const soketIO = require("socket.io")(http, {
+        cors:{
+            origin: "http://localhost:5173"
+        }
+    })
+
+    soketIO.on("connection", (socket)=>{
+        // console.log(">>>>>>>", socket, ">>>>>>>>>")
+
+        socket.on("oi", (data)=>{
+            console.log(data, "ikuzoooooooooooooo")
+        })
+
+        socket.on("encount", (data)=>{
+            
+            if(data === "hit"){
+
+                socket.emit("screenSwitch", "hit");
+            }
+            
+        })
+
+        socket.on("back", (data)=>{
+            if(data=="backback"){
+
+                socket.emit("backSwitch", "backback")
+                console.log("kaettekitaOoo")
+            }
+        })
+
+        socket.on("save",(data)=>{
+            console.log(data, "rthis")
+            socket.emit("save", "save")
+        })
+
+
+    })
+    
+
+}
